d7da3295b6a26391d263cf2ce6a151b28862f0d5
David Tewkesbury <tewkesburyd@hotmail.com>
2022-11-02 20:44:03 -0600

    commit tree cd78377261fc03ec1607797d1031ae4460600303

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+const dodger = document.querySelector('#dodger');
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "arrowLeft"){
+        moveDodgerLeft()
+        console.log('left')
+    } 
+    else if (event.key === "arrowRight"){
+        moveDodgerRight()
+        console.log('right')
+    }
+})
+
+
+const moveDodgerLeft = () =>{
+        const leftNumbers = dodger.style.left.replace('px', '');
+        const left = parseInt(leftNumbers, 10);
+
+        dodger.style.left = `${left - 1}px`
+        }
+
+const moveDodgerRight = () => {
+        const leftNumbers = dodger.style.left.replace('px', '');
+        const left = parseInt(leftNumbers, 10);
+
+        dodger.style.left = `${left + 1}px`
+        }
+
+//moveDodgerLeft()
